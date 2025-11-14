3b55ba0231682a25de7c58638db4d8e724ba4349
JennaBHA <benhadj.jenna@gmail.com>
2025-11-14 15:58:02 +0100

    Fichier Jenna

--- shop.backend/src/main/java/com/api/shop/backend/repository/UserRepository.java
+++ shop.backend/src/main/java/com/api/shop/backend/repository/UserRepository.java
@@ -1,4 +1,10 @@
 package com.api.shop.backend.repository;
 
-public interface UserRepository {
+import com.api.shop.backend.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
 }
